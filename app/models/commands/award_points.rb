eb9c221cc91f8f146866487651d2765c0c5c6d15
Jason Noble <github+jasonn@jasonnoble.org>
2015-09-14 13:02:45 -0600

    Add logging

--- app/models/commands/award_points.rb
+++ app/models/commands/award_points.rb
@@ -14,6 +14,7 @@ class Commands::AwardPoints < Commands::Base
   end
 
   def posse
+    Rails.logger.info posse_name
     if posse_name.to_s.start_with?("<@")
       Student.find_by(slack_uid: posse_name.match(/<@(.+)>/)[1]).try(:posse)
     else
